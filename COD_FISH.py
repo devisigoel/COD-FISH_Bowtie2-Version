2be62fc5fb994249e4fe6d09932659ea105bb89e
ryandikdan <65059714+ryandikdan@users.noreply.github.com>
2022-07-08 11:53:12 -0400

    fixing config file dynamic vs. Greedy

--- COD_FISH.py
+++ COD_FISH.py
@@ -230,9 +230,9 @@ else:
         f.write("Heterodimer_melting_temperature_estimation_method = Primer3\n")
     
     if advanced_flag:
-        f.write("Probe_set_selection_method = " + str(input("Please enter Dynamic Programming or Greedy Method to indicate which probe selection selection you would like to use.")))
+        f.write("Probe_set_selection_method = " + str(input("Please enter 'Dynamic' or 'Greedy' to indicate which probe selection selection you would like to use.")))
     else:
-        f.write("Probe_set_selection_method = Greedy Method\n")
+        f.write("Probe_set_selection_method = \"Greedy\"\n")
 
     f.write("#Filter repeating nucleotides, AAAAA, GGGG, CCCC, TTTTT\n")
     f.write("filter_repeats = True\n")
@@ -376,9 +376,9 @@ for transcript in transcript_targets:
     print('Probe set selection...')
     # Sort probes by their specificity and then remove overlapping probes
 
-    if config.Probe_set_selection_method == "Greedy Method":
+    if config.Probe_set_selection_method == "Greedy":
         final_probes, total_score_greedy = utils.select_nonoverlapping_probes(offtarget_Tm_scores_list, probe_len, num_probes)
-    elif config.Probe_set_selection_method == "Dynamic Programming":
+    elif config.Probe_set_selection_method == "Dynamic":
         print(transcript + "is choosing Dynamic Programming")
         final_probes_greedy, total_score_greedy = utils.select_nonoverlapping_probes(offtarget_Tm_scores_list, probe_len, num_probes)
         final_probes_ts, total_score_ts = utils.find_optimal_set(offtarget_Tm_scores_list, probe_len + 2, num_probes, final_probes_greedy, beam=2.0)
